b853cfb8d7e2edf1e8d93c70ca888bcb583ee7f8
svyatou <vladimir-tarasov-2002@mail.ru>
2020-05-08 17:57:17 +0500

    Update

---
+++ app/src/main/java/com/example/gridsubmarine/DialogNewMulti.java
@@ -0,0 +1,21 @@
+package com.example.gridsubmarine;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogNewMulti extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View dialogView = inflater.inflate(R.layout.layout_dialog, null);
+        return builder.create();
+    }
+    }
--- app/src/main/java/com/example/gridsubmarine/MultiPlayerGame.java
+++ app/src/main/java/com/example/gridsubmarine/MultiPlayerGame.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MultiPlayerGame extends AppCompatActivity {
 
+    private TextView textView;
+    public final int MAX = 34;
+    public int number;
+    int score = 0;
+    private TextView textScoremain;
     Button multiPlayer;
 
     @Override
@@ -19,7 +32,9 @@ public class MultiPlayerGame extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-//        startGame();
+
+        initeGameMulti();
+
         if (Build.VERSION.SDK_INT < 19) {
             View v = this.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
@@ -30,5 +45,61 @@ public class MultiPlayerGame extends AppCompatActivity {
             decorView.setSystemUiVisibility(uiOptions);
         }
 
+    }
+
+    void initeGameMulti(){
+            setContentView(R.layout.activity_main);
+
+            GridView gridView = (GridView)findViewById(R.id.gridView);
+            ImageAdapter imageAdapter = new ImageAdapter(this);
+            gridView.setAdapter(new ImageAdapter(this));
+            gridView.setOnItemClickListener(gridviewOnItemClickListener);
+
+            score = 0;
+
+            textScoremain = (TextView)findViewById(R.id.textScoremain);
+            textScoremain.setText("Количество попыток: "+ String.valueOf(score));
+
+            Random random = new Random();
+            number = random.nextInt(MAX);
+        }
+    private GridView.OnItemClickListener gridviewOnItemClickListener = new GridView.OnItemClickListener(){
+
+        //        Выводится номер позиции.
+        @Override
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+
+            score++;
+
+            textScoremain = (TextView)findViewById(R.id.textScoremain);
+            textScoremain.setText("Количество попыток: "+ String.valueOf(score));
+
+
+            textView = (TextView)findViewById(R.id.info);
+
+            Animation anim = new AlphaAnimation(0.0f, 1.0f);
+            anim.setDuration(1000);
+            anim.setStartOffset(20);
+            textView.startAnimation(anim);
+
+            textView.setText("Ваш выбор: " +String.valueOf(position+1));
+
+            if (number == position) {
+                Boom();
+            } else if(number != position){
+
+                ImageView viewI = (ImageView)v;
+                Animation anim1 = new AlphaAnimation(0.0f, 1.0f);
+                anim1.setDuration(1000);
+                anim1.setStartOffset(20);
+                viewI.setImageResource(R.drawable.no);
+                viewI.startAnimation(anim1);
+            }
+        }
+    };
+
+    void Boom(){
+        DialogNewMulti dialog = new DialogNewMulti();
+        dialog.show(getSupportFragmentManager(), "");
     }
 }
